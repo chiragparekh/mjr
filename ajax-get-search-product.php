d73be8980fd0d82750a50bac92a91638bad3a13b
kishanpatadia <patadikishan@gmail.com>
2013-12-05 01:48:18 +0530

    ajax loading implemention in product search...

--- ajax-get-search-product.php
+++ ajax-get-search-product.php
@@ -116,7 +116,8 @@ if ($records > 0) {
         <div class="product">
             <div class="pro-heading"><h1 class="center"><?php echo ucwords($row['name']); ?></h1></div>
             <div class="pro-img"><a rel="example_group" href="manager/uploads/original/<?php echo $row["sub_cat_name"]; ?>/<?php echo $row["image_path"]; ?>" title=""><img src="manager/uploads/thumbs/<?php echo $row["sub_cat_name"]; ?>/<?php echo $row["image_path"]; ?>" width="180" height="160" alt="" /></a></div>
-            <div class="pro-detail"><a href="product.php?q=<?php echo $row["pid"] ?>">View Detail</a><a href="product.php">Add to Cart</a></div>
+            <div class="pro-detail"><a href="product.php?q=<?php echo $row["pid"] ?>">View Detail</a>
+                <a href="product.php?q=<?php echo $row["pid"] ?>">Add to Cart</a></div>
         </div>
         <?php
     }
