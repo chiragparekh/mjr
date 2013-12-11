bb32f85c21ff1885f6208db836f45762b0c88fde
kishanpatadia <patadiakishan@gmail.com>
2013-12-11 23:33:13 +0530

    - google profile link added in footer...

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 </div>
 <div id="footer">
-    <span>MJR Jewels &COPY; 2013-2014. All rights reserved. By Nirav Patadiya, Kishan Patadia and Chirag Parekh</span>
+    <span>MJR Jewels &COPY; 2013-2014. All rights reserved. By <a href="https://plus.google.com/106815413559094250643/about">Nirav Patadiya</a>, <a href="https://plus.google.com/108317193603739769059/about">Kishan Patadia</a> and <a href="https://plus.google.com/110531912725431692577/about">Chirag Parekh</a></span>
   </div>
 </div>
